cddeed3ea7d0368c9e859b52f85c8eff4236ab81
ukitinu <ukitinu@gmail.com>
2022-01-15 17:12:26 +0100

    Add logging to FileReader

--- src/main/java/ukitinu/markovwords/readers/FileReader.java
+++ src/main/java/ukitinu/markovwords/readers/FileReader.java
@@ -1,17 +1,22 @@
 package ukitinu.markovwords.readers;
 
+import ukitinu.markovwords.lib.Logger;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileReader implements Reader {
+    private static final Logger LOG = Logger.create(FileReader.class);
+
     @Override
     public String read(String src) {
         try {
             var path = Path.of(src);
             return Files.readString(path, StandardCharsets.UTF_8);
         } catch (IOException e) {
+            LOG.error("Unable to read {}: {}", src, e.getMessage());
             return "";
         }
     }
